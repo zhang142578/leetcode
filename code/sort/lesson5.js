f7e7d76fe1a3fc2ab02681b723fb23d2eeaf124e
zhang142578 <zhty4082@qq.com>
2019-04-29 15:55:14 +0800

    gengxin

--- code/sort/lesson5.js
+++ code/sort/lesson5.js
@@ -1,21 +1,21 @@
-export default (arr) => {
-  let quickSort = (arr) => {
-    if (arr.length < 2) {
-      return arr
-    } else {
-      let flag = arr[0]
-      let left = []
-      let right = []
-      for (let i = 1; i < arr.length; i++) {
-        let tmp = arr[i]
-        if (arr[i] < flag) {
-          left.push(tmp)
-        } else {
-          right.push(tmp)
-        }
-      }
-      return quickSort(left).concat(flag, quickSort(right))
-    }
-  }
-  return quickSort(arr)
-}
+// export default (arr) => {
+//   let quickSort = (arr) => {
+//     if (arr.length < 2) {
+//       return arr
+//     } else {
+//       let flag = arr[0]
+//       let left = []
+//       let right = []
+//       for (let i = 1; i < arr.length; i++) {
+//         let tmp = arr[i]
+//         if (arr[i] < flag) {
+//           left.push(tmp)
+//         } else {
+//           right.push(tmp)
+//         }
+//       }
+//       return quickSort(left).concat(flag, quickSort(right))
+//     }
+//   }
+//   return quickSort(arr)
+// }
